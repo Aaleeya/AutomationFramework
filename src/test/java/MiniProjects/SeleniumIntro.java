ba54b980dbb87d2288ea52bb27a9ecacddfa456e
Aaleeya <shaikhaaleeya17@gmail.com>
2025-04-12 14:50:33 +0530

    code added

---
+++ src/test/java/MiniProjects/SeleniumIntro.java
@@ -0,0 +1,20 @@
+package MiniProjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumIntro {
+
+    public static void main(String[] args) {
+
+        //Invoking Browser
+        //Chrome-> ChromeDriver -> Methods
+        //WebDriver interface which sets the rules for all browser
+        //group of methods with no implemention- classes implement the methods
+        //WebDriver methods + browser specific class methods =
+        // ChromeDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(); // we want to create an object which only invokes webdriver implemented methods
+
+
+    }
+}
