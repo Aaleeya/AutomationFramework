4f2e0432d5d6cbdd8f02fd6d67dab66fa5ac0f19
Aaleeya <shaikhaaleeya17@gmail.com>
2025-04-27 21:26:40 +0530

    Automated ParaBank Login page

---
+++ src/test/java/MiniProjects/ParaBankLogin.java
@@ -0,0 +1,19 @@
+package MiniProjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ParaBankLogin {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+        driver.manage().window().maximize();
+        driver.findElement(By.name("username")).sendKeys("TestCustomer");
+        driver.findElement(By.cssSelector("input[type='password']")).sendKeys("Banking123");
+        driver.findElement(By.cssSelector("input[value='Log In']")).click();
+
+        driver.quit();
+    }
+}
